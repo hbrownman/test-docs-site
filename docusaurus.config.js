5108e56f4e3b54a9db2a0be27b7d14084695d7ac
hbrownman <hbrownman12@yahoo.com>
2024-07-15 22:01:04 -0700

    change title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Hello this is on a different github account',
+  tagline: 'Dinosaurs are cool account man',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
